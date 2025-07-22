3c570db8088e705a41ce93a4c84cdef8b5ef16b3
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-22 17:06:26 +0300

    Operator in Addition

---
+++ Operator/Operator.py
@@ -0,0 +1,7 @@
+operator= input("Enter the operator (+ - * /): ")
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the first number: "))
+
+if operator== "+":
+    result=num1 + num2
+    print(round(result,3))
