53e846fced19baf1bbf8ee6117b1be1df3afb571
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-27 19:47:05 +0300

    data types

---
+++ Variables_Data_Types/primitive_data_type.py
@@ -0,0 +1,29 @@
+# What is Variable 
+"""
+Variable is a name that points to an object stored in memory.
+
+A variable is a named container that stores data in memory. 
+It allows you to assign a value (like a number, text, or object) to a name, which you can reuse later in your code.
+
+
+"""
+
+x=5
+y="Hello"
+z=5.5
+f=True
+
+
+"""
+Knowing the data type
+type()
+<class 'int'>
+<class 'str'>
+<class 'float'>
+<class 'bool'> 
+"""
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(f))
