2313cbf617826e3902f18212df59bb629a341254
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-27 20:21:10 +0300

    Literals and Constant

---
+++ Variables_Data_Types/literalsConstant.py
@@ -0,0 +1,16 @@
+"""
+Literals in Python are fixed values assigned to variables or used directly in code.
+They represent constant values that do not change during program execution.
+
+"""
+x="hello"
+y=10
+
+"""
+constant Variables meant to hold unchanging values (by convention).
+
+"""
+
+PI=3.14
+MAX_USERS=1000
+
