534ec1f07b9bfc9105d2d8e15117f41c28556929
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-27 20:37:55 +0300

    base converstion

---
+++ BaseConverstion.py
@@ -0,0 +1,20 @@
+"""
+Changing The Number system 
+1. Octa 
+2. Hexa
+3. binary 
+4. decimal 
+
+"""
+
+base10=456
+
+# converting this Decimal 
+# binary bin()
+# Hexa   hex()
+#octa    oct()
+
+print(hex(base10))
+print(oct(base10))
+print(bin(base10))
+
