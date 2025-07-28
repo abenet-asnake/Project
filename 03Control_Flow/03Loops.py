23a3437aaf608d1202f2c171b629c440f08b5efc
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-28 18:35:18 +0300

    for loops

---
+++ 03Control_Flow/03Loops.py
@@ -0,0 +1,14 @@
+# =============================================
+#           COMPLETE LOOP GUIDE IN PYTHON
+#for loop, while loop, nested loop, break, continue, pass
+# =============================================
+# 1. For Loop
+print("\n1. For Loop:") 
+for i in range(5):
+    print("Iteration", i)   # Output: Iteration 0 to 4  
+
+# Practical Example
+print("\nPractical Example:")       
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print("Fruit:", fruit)  # Output: Fruit: apple, banana, cherry
