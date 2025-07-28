58e6a63e3260f905788c5ad22e86501220884398
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-28 18:36:10 +0300

    while  loops

--- 03Control_Flow/03Loops.py
+++ 03Control_Flow/03Loops.py
@@ -12,3 +12,16 @@ print("\nPractical Example:")
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print("Fruit:", fruit)  # Output: Fruit: apple, banana, cherry
+# 2. While Loop
+print("\n2. While Loop:")   
+count = 0
+while count < 5:
+    print("Count:", count)  # Output: Count: 0 to 4
+    count += 1              
+# Practical Example
+print("\nPractical Example:")       
+number = 0
+while number < 10:
+    print("Number:", number)  # Output: Number: 0 to 9
+    number += 2  # Increment by 2   
+    
