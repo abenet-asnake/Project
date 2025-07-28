3f8ce104c1addf87cfd9bc413062f404a9fe492e
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-07-28 18:10:49 +0300

    conditional statment if

---
+++ 03Control_Flow/01Conditional.py
@@ -0,0 +1,10 @@
+# =============================================
+#           CONDITIONAL STATEMENTS IN PYTHON
+# Conditional statements allow you to execute code based on certain conditions.
+# =============================================
+
+# 1. Basic If Statement
+age = 20
+if age >= 18:
+    print("You are an adult.")  # This will execute if age is 18 or older   
+
