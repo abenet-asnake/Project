063a563f99ac2ee56bb9204532803efb0d57a447
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-08-01 10:50:48 +0300

    variable keyword arguments

--- 04Function/05VariableArgument.py
+++ 04Function/05VariableArgument.py
@@ -25,3 +25,29 @@ def calculate_total_with_annotations(*prices: float) -> float:
 print(calculate_total_with_annotations(10.99, 5.49, 3.25))  # Output: 19.73
 
 
+"""
+=====================================================================
+2. kwargs (Variable Keyword Arguments)
+What it is
+Collects extra keyword arguments into a dictionary
+
+The ** unpacks key-value pairs (name kwargs is convention)
+
+
+======================================================================
+
+"""
+
+def create_user_profile(**user_info):
+    profile = {
+        'username': user_info.get('username', 'Guest'),
+        'email': user_info.get('email'),
+        'premium': user_info.get('is_premium', False)
+    }
+
+    return profile
+# Calling the function with variable keyword arguments
+print(create_user_profile(username="abenet_asnake", 
+                          email="abenet.asnaketesfaye@gmail.com",
+                           is_premium=True,
+                           joined="2023-10-01"))
