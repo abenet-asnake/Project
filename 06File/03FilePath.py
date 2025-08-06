e2822a1e4205a7a7684436a406d206fc2f255e7a
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-08-06 14:51:10 +0300

    using os.path checking the existane of the file

--- 06File/03FilePath.py
+++ 06File/03FilePath.py
@@ -10,3 +10,12 @@
 
 import os
 from pathlib import Path
+# Your file paths (using raw strings for Windows paths)
+csv_path = r"c:/Users/KIIT01/Desktop/Project BOX/Pythons/Project/students.csv"
+text_path = r"c:/Users/KIIT01/Desktop/Project BOX/Pythons/Project/06File/data.text"
+
+
+# using os.path to define a file path
+print("=== Using os.path ===")
+print("CSV file exists:", os.path.exists(csv_path))  # Check if files exist
+print("Text file exists:", os.path.exists(text_path)) # Check if files exist
