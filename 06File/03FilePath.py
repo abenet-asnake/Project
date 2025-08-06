aaf3895afdf9006167184b7a05eb2e763ef2693b
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-08-06 21:16:25 +0300

    get the current working directory dirname, basename

--- 06File/03FilePath.py
+++ 06File/03FilePath.py
@@ -19,3 +19,12 @@ text_path = r"c:/Users/KIIT01/Desktop/Project BOX/Pythons/Project/06File/data.te
 print("=== Using os.path ===")
 print("CSV file exists:", os.path.exists(csv_path))  # Check if files exist
 print("Text file exists:", os.path.exists(text_path)) # Check if files exist
+
+# get the current working directory
+current_directory = os.getcwd()
+print("Current working directory:", current_directory)  # Display current directory
+
+text_directory = os.path.dirname(text_path)  # Get directory of the text file
+print("Text file directory:", text_directory)  # Display directory of the text file
+text_file_name = os.path.basename(text_path)  # Get the file name from the path
+print("Text file name:", text_file_name)  # Display the file name
