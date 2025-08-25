c1f15765ee2714f1369156947045fb6f63070a89
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-08-25 08:43:18 +0300

    it shows or print out inherted method

---
+++ 08OOP/inheritance.py
@@ -0,0 +1,31 @@
+"""
+Inheritance Example
+"""
+
+class Person:
+    def __init__(self, name,age):
+        self.name=name
+        self.age=age
+
+    def display(self):
+        print(f"Name:{self.name}, Age:{self.age}")
+
+
+class Student (Person):
+    # Student inherits a person 
+    def __init__(self, name, age,studentID):
+        super().__init__(name, age)
+        self.studentID=studentID
+    
+    def show_student(self):
+        print(f"Student ID={self.studentID}")
+
+
+# it Ceareting an object 
+
+student1=Student("Abenet",32,"FIS\1234")
+
+# it shows or print out inherted method 
+student1.display()
+
+Stu
