a79ca8575879186310afc22b1bf0953a4b54ee03
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2025-08-29 08:57:13 +0300

    implementing the promse class in the shape

--- 08OOP/abstractClass.py
+++ 08OOP/abstractClass.py
@@ -17,3 +17,13 @@ class AbstractShape(ABC):
         pass
 
 
+class Circle(AbstractShape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
